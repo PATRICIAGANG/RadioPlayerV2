0c73f3413a2931e2282225ad58960799587b8588
PATRICIAGANG <85884951+PATRICIAGANG@users.noreply.github.com>
2021-07-17 21:50:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,17 +21,17 @@ import re
 
 
 class Config:
-    ADMIN = os.environ.get("ADMINS", "1316963576")
+    ADMIN = os.environ.get("ADMINS", "1832447570")
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
-    API_ID = int(os.environ.get("API_ID", 12345))
-    CHAT = int(os.environ.get("CHAT", ""))
+    API_ID = int(os.environ.get("API_ID", "3898418"))
+    CHAT = int(os.environ.get("CHAT", "-1001489244957"))
     LOG_GROUP = os.environ.get("LOG_GROUP", "")
     if LOG_GROUP:
         LOG_GROUP = int(LOG_GROUP)
     else:
         LOG_GROUP = None
     STREAM_URL = os.environ.get("STREAM_URL", "https://radioindia.net/radio/hungamanow/icecast.audio")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    SESSION = os.environ.get("SESSION_STRING", "")
+    API_HASH = os.environ.get("API_HASH", "5a82313211da04d63297bd4de436228c")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1645277361:AAG15fdypo92MVNP-73flxcUgmJjTLi9BHw") 
+    SESSION = os.environ.get("SESSION_STRING", "BQAXiBfd_2Cfe1s_K_ajo8XGsmKwwX4eYSe1EG1nhGXbLFkelgbdlJH-wRciDRZq6lYTDfXlGVEhE_qJf4WMKIh5SzhOR_m8Kr00hBqWcAZqFT5ynKsKEhnKgR_dIvuCqcu7-dwbtiCNvmG7xpL4N1ek6MP3QUA7ON74E6w9Gdr3MVpuPbwkDpdmc0CtRs-W0h67I-k_T8mTgGviRBA82M1XQfc8_cw7ZbPb28dN3Xa3l8DNSkBzNn8p-gZ_np4c7P50HDUlkiIZd9rfv_Wt_KVxv2uawqeBUtYYRB1jeiUw8Or1-cKNU0m5XO9GmJsoBUEuULrB_2j1fq-wV7rBalI0X9XNvAA")
 
